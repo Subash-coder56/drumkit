cfbbab4dfd98360fdf73f1cb53255cc9c441e169
Subash-coder56 <subash15461645@gmail.com>
2024-10-02 00:21:58 -0700

    qwertyuiop

--- index.js
+++ index.js
@@ -28,43 +28,43 @@ document.addEventListener("keypress",function(event){
 function makesound(key){
     switch (key) {
         case "D":
-            var tom1 = new Audio("/Drum Kit Starting Files/sounds/tom-1.mp3");
+            var tom1 = new Audio("/tom-1.mp3");
             tom1.play();
     
             
         break;
         case "R":
-            var tom2 = new Audio("/Drum Kit Starting Files/sounds/tom-2.mp3");
+            var tom2 = new Audio("/tom-2.mp3");
             tom2.play();
     
             
         break;
         case "U":
-            var tom3 = new Audio("/Drum Kit Starting Files/sounds/tom-3.mp3");
+            var tom3 = new Audio("/tom-3.mp3");
             tom3.play();
     
             
         break;
         case "M":
-            var tom4 = new Audio("/Drum Kit Starting Files/sounds/tom-4.mp3");
+            var tom4 = new Audio("/tom-4.mp3");
             tom4.play();
     
             
         break;
         case "K":
-            var crash = new Audio("/Drum Kit Starting Files/sounds/crash.mp3");
+            var crash = new Audio("/crash.mp3");
             crash.play();
     
             
         break;
         case "I":
-            var kick = new Audio("/Drum Kit Starting Files/sounds/kick-bass.mp3");
+            var kick = new Audio("/kick-bass.mp3");
             kick.play();
     
             
         break;
         case "T":
-            var snare = new Audio("/Drum Kit Starting Files/sounds/snare.mp3");
+            var snare = new Audio("/crash.mp3");
             snare.play();
     
             
